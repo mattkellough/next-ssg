f939e2e289458d3dcdc2d436a95015408374b5d5
Matthew Kellough <mkellough@magicleap.com>
2020-06-01 14:50:29 -0400

    date

--- components/Layout.js
+++ components/Layout.js
@@ -1,36 +1,22 @@
 import React from "react";
 import Head from "next/head";
-import moment from "moment";
 import Link from "next/link";
 
-const Layout = ({ date, children }) => {
+const Layout = ({ children }) => {
   return (
     <>
       <Head>
-        <title>SSG App - {date}</title>
+        <title>SSG App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <header>
         <Link href="/">
           <a>Home</a>
         </Link>
-        <div>
-          <p>Time Rendered: {date}</p>
-        </div>
       </header>
       <main>{children}</main>
     </>
   );
-};
-
-export const getStaticProps = () => {
-  const date = moment().utcOffset("-0400").format("MMMM Do YYYY, h:mm:ss a");
-
-  return {
-    props: {
-      date,
-    },
-  };
 };
 
 export default Layout;
